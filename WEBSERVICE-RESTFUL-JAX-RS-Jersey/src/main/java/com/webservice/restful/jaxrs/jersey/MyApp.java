1d33efef74b5d067794d499fac557f79c538d8ec
manjitwaikhom <manjitsinghwk@gmail.com>
2019-07-02 19:48:46 +0530

    getClasses()

--- WEBSERVICE-RESTFUL-JAX-RS-Jersey/src/main/java/com/webservice/restful/jaxrs/jersey/MyApp.java
+++ WEBSERVICE-RESTFUL-JAX-RS-Jersey/src/main/java/com/webservice/restful/jaxrs/jersey/MyApp.java
@@ -1,9 +1,18 @@
 package com.webservice.restful.jaxrs.jersey;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 //if you use this class you dont need to use web.xml but u must use servlet 3 
 @ApplicationPath("messageapi")
 public class MyApp extends Application{
 
+    /*override this class and provide a set of allowed classes 
+     * @Override
+    public Set<Class<?>> getClasses() {
+    Set<Class<?>> listOfAllowedClasses=new HashSet<>();
+        return listOfAllowedClasses;
+    }*/
 }
