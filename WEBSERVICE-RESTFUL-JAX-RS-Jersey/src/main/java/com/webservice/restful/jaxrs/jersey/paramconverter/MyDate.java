5cadc9087c78300d5cd386d2051056a504eccf36
manjitwaikhom <manjitsinghwk@gmail.com>
2019-07-10 12:58:29 +0530

    implementing Custom ParamConverters

---
+++ WEBSERVICE-RESTFUL-JAX-RS-Jersey/src/main/java/com/webservice/restful/jaxrs/jersey/paramconverter/MyDate.java
@@ -0,0 +1,31 @@
+package com.webservice.restful.jaxrs.jersey.paramconverter;
+
+public class MyDate {
+    int date;
+    int month;
+    int year;
+    public int getDate() {
+        return date;
+    }
+    public void setDate(int date) {
+        this.date = date;
+    }
+    public int getMonth() {
+        return month;
+    }
+    public void setMonth(int month) {
+        this.month = month;
+    }
+    public int getYear() {
+        return year;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+    @Override
+    public String toString() {
+	return "MyDate [date=" + date + ", month=" + month + ", year=" + year + "]";
+    }
+    
+    
+}
