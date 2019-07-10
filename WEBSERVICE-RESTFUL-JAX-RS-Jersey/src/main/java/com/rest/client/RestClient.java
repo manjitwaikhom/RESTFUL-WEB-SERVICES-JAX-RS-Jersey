5c68062d05b0f0eae645a53428f33e9128f7230c
manjitwaikhom <manjitsinghwk@gmail.com>
2019-07-10 20:07:14 +0530

    webservice proper creation of url

--- WEBSERVICE-RESTFUL-JAX-RS-Jersey/src/main/java/com/rest/client/RestClient.java
+++ WEBSERVICE-RESTFUL-JAX-RS-Jersey/src/main/java/com/rest/client/RestClient.java
@@ -2,6 +2,7 @@ package com.rest.client;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
 
 import com.webservice.restful.jaxrs.jersey.model.Message;
@@ -10,10 +11,20 @@ public class RestClient {
 
     public static void main(String[] args) {
 	Client client=ClientBuilder.newClient();
-	String uri="http://localhost:8080/WEBSERVICE-RESTFUL-JAX-RS-Jersey/messageapi/messages/1";
+	WebTarget baseTarget=client.target("http://localhost:8080/WEBSERVICE-RESTFUL-JAX-RS-Jersey/messageapi/");
+	WebTarget messageTarget = baseTarget.path("messages");
+	WebTarget singleMessageTarget = messageTarget.path("{messageId}");
+	
+	Message message1=singleMessageTarget.resolveTemplate("messageId", "1").request().get(Message.class);
+	System.out.println(message1.getMessage());
+	
+	Message message2=singleMessageTarget.resolveTemplate("messageId", "2").request().get(Message.class);
+	System.out.println(message2.getMessage());
+	
+	/*String uri="http://localhost:8080/WEBSERVICE-RESTFUL-JAX-RS-Jersey/messageapi/messages/1";
 	Response response=client.target(uri).request().get();
 	Message message=response.readEntity(Message.class);
-	System.out.println(message.getMessage());
+	System.out.println(message.getMessage());*/
 	
     }
 
